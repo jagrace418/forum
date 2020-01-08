b68c4756d9e96344af955ac752dc796935642d71
jagrace <jagrace418@gmail.com>
2020-01-07 20:55:31 -0600

    E12: more channel form validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,8 @@
 				<ul class="navbar-nav mr-auto">
 					<a class="nav-link" href="/threads">All Threads</a>
 
+					<a class="nav-link" href="/threads/create">New Thread</a>
+
 					<li class="nav-item dropdown">
 						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
 						   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
